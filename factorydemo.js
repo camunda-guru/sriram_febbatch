dcae2fc6a27d88f6b7cd71b1b7b2d62c1b285ffd
eswaribala <Parameswaribala@gmail.com>
2017-03-03 17:26:22 +0530

    Add files via upload

---
+++ factorydemo.js
@@ -0,0 +1,21 @@
+﻿var factoryModule = angular.module('FactoryModule', ['ServiceModule']);
+factoryModule.factory('dashboardFact', ['$injector',function ($injector) {
+    obj = {};
+    obj.findService=function(option)
+    {
+        obj = {};
+        angular.module('ServiceModule')['_invokeQueue'].forEach(function (value) {
+
+            if(value[2][0]==option+'Service')
+            {
+                console.log("Matching....", value[2][0]);
+                obj = $injector.get(value[2][0]);
+            }
+
+        });
+        return obj;
+
+    }
+    return obj;
+
+}]);
---
+++ servicedemo.js
@@ -0,0 +1,74 @@
+﻿var serviceModule = angular.module('ServiceModule', []);
+serviceModule.service('WayPointService', ['$http',function ($http) {
+
+    var wayPointList=function()
+    {
+        return $http({
+            method: 'GET',
+            dataType: "jsonp",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+
+            },
+            url: 'https://maps.googleapis.com/maps/api/directions/json?origin=Chennai&destination=Pune'
+        }).then(function (response) {
+            return response;
+        });
+    }
+    return {
+        WayPointServiceObj:wayPointList
+    }
+
+
+}]);
+
+
+
+
+serviceModule.service('FinancialService', ['$http',function ($http) {
+
+    var financialList = function () {
+        return $http({
+            method: 'GET',
+            dataType: "jsonp",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+
+            },
+            url: 'https://www.quandl.com/api/v3/datasets/WIKI/FB.json'
+        }).then(function (response) {
+            return response;
+        });
+    }
+    return {
+        FinancialServiceObj: financialList
+    }
+
+
+
+
+
+}]);
+serviceModule.service('GeoCodeService', ['$http', function ($http) {
+
+    var geoCodeList = function () {
+        return $http({
+            method: 'GET',
+            dataType: "jsonp",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+
+            },
+            url: 'https://maps.googleapis.com/maps/api/geocode/json?address=600049'
+        }).then(function (response) {
+            return response;
+        });
+    }
+    return {
+        GeoCodeServiceObj: geoCodeList
+    }
+
+
+
+
+}]);
